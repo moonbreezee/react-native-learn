c42ed96d7818bf101e9cc70737864f125803caee
qiuwww <1014913222@qq.com>
2020-12-09 10:16:59 +0800

    update

--- NativeBase-master/src/init.js
+++
@@ -1,9 +0,0 @@
-import { Theme } from "native-base-shoutem-theme";
-// 组件的基本结构配置
-import getThemeStyle from "./theme/components";
-
-// 设置默认的主题，样式配置
-export default function setDefaultThemeStyle() {
-  const theme = getThemeStyle();
-  Theme.setDefaultThemeStyle(theme);
-}
